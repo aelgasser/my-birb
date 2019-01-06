77b39759e481f0ad99461d612c887dfb05592c27
aelgasser <abderraouf.elgasser@gmail.com>
2019-01-05 23:35:48 -0800

    Moved the post items into a separate widget

---
+++ app/test/post_item_test.dart
@@ -0,0 +1,14 @@
+import 'package:birb/post_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Renders a post', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: PostItem(),
+    ));
+
+    expect(find.byType(Card), findsOneWidget);
+    expect(find.text('Prim Birb'), findsOneWidget);
+  });  
+}
